b2e810cd7c488ea37212da65400df2af42750482
kangmeoungoun <kes98202@naver.com>
2021-02-12 09:46:09 +0900

    공통 인터페이스 적용

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ##### 공통 인터페이스 기능
-### 공통 인터페이스 설정
+### 공통 인터페이스 적용
 
-![image](https://user-images.githubusercontent.com/40969203/107716402-ac27e000-6d14-11eb-8713-235f226b37f4.png)
-![image](https://user-images.githubusercontent.com/40969203/107716408-b0ec9400-6d14-11eb-9568-1935a0ff1f5a.png)
+![image](https://user-images.githubusercontent.com/40969203/107717450-01fd8780-6d17-11eb-930c-8d5550fc3ed0.png)
+![image](https://user-images.githubusercontent.com/40969203/107717458-0629a500-6d17-11eb-908d-92b5b26449e5.png)
+![image](https://user-images.githubusercontent.com/40969203/107717464-09249580-6d17-11eb-93f6-0afc80169c84.png)
+![image](https://user-images.githubusercontent.com/40969203/107717472-0e81e000-6d17-11eb-8acd-042301ab6bd6.png)
+![image](https://user-images.githubusercontent.com/40969203/107717482-15a8ee00-6d17-11eb-82dd-270cff01b63a.png)
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -22,7 +23,6 @@ class MemberRepositoryTest{
 
     @Test
     void testMember(){
-        System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
         Member member = new Member("memberA");
         Member saveMember = memberRepository.save(member);
         Optional<Member> byId = memberRepository.findById(saveMember.getId());
@@ -30,6 +30,30 @@ class MemberRepositoryTest{
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
-
     }
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //단검 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(2).isEqualTo(all.size());
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(2).isEqualTo(count);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        assertThat(0).isEqualTo(deletedCount);
+    }
+    
 }
