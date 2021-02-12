5d71d5035c7e50f0d5fdf112a2037a5fb03aaf67
kangmeoungoun <kes98202@naver.com>
2021-02-12 19:15:09 +0900

    스프링 데이터 JPA 페이징과 정렬

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ##### 쿼리 메소드 기능
-### 반환 타입
+### 스프링 데이터 JPA 페이징과 정렬
 
-jpa 는 singleresult 결과 없을시  noresultException 이 터지는데
-data-jpa 는 결과 없으면 null 반환
-
-![image](https://user-images.githubusercontent.com/40969203/107749382-d782ed00-6d5d-11eb-953f-7940430403f2.png)
-![image](https://user-images.githubusercontent.com/40969203/107749390-dbaf0a80-6d5d-11eb-8467-21cabb71d00b.png)
+![image](https://user-images.githubusercontent.com/40969203/107755637-727fc500-6d66-11eb-8c56-a53d7983f9dd.png)
+![image](https://user-images.githubusercontent.com/40969203/107755642-757ab580-6d66-11eb-89db-3585bbb6fcb9.png)
+![image](https://user-images.githubusercontent.com/40969203/107755656-7875a600-6d66-11eb-9baf-7b8f6d7e16f4.png)
+![image](https://user-images.githubusercontent.com/40969203/107755682-7f9cb400-6d66-11eb-8303-6ecc69808db3.png)
+![image](https://user-images.githubusercontent.com/40969203/107755695-8297a480-6d66-11eb-8565-28daf3d79275.png)
+![image](https://user-images.githubusercontent.com/40969203/107755708-862b2b80-6d66-11eb-96ad-82dc9e0d49a3.png)
+![image](https://user-images.githubusercontent.com/40969203/107755718-89beb280-6d66-11eb-95d3-6f35acc2b80a.png)
+![image](https://user-images.githubusercontent.com/40969203/107755729-8dead000-6d66-11eb-9f0b-acae9405ad7b.png)
+![image](https://user-images.githubusercontent.com/40969203/107755740-90e5c080-6d66-11eb-9bc7-a5c61b940555.png)
+![image](https://user-images.githubusercontent.com/40969203/107755754-94794780-6d66-11eb-8872-659f5eb48729.png)
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -16,6 +19,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -73,6 +77,7 @@ class MemberRepositoryTest{
     }
     @Test
     void findHelloBy(){
+
         memberRepository.findTop3HelloBy();
     }
     @Test
@@ -142,6 +147,34 @@ class MemberRepositoryTest{
         Optional<Member> findMember = memberRepository.findOptionalByUsername("sfdsfsdf");
         System.out.println("findMember = " + findMember);
     }       
+    @Test
+    void paging(){
+        //given
+        memberRepository.save(new Member("member1" , 10));
+        memberRepository.save(new Member("member2" , 10));
+        memberRepository.save(new Member("member3" , 10));
+        memberRepository.save(new Member("member4" , 10));
+        memberRepository.save(new Member("member5" , 10));
+        int age =10;
+        int offset = 0;
+        int limit =3;
 
+        PageRequest pageRequest = PageRequest.of(0 , 3 , Sort.by(Sort.Direction.DESC , "username"));
+        //when
+        //Page<Member> page = memberRepository.findByAge(age , pageRequest);
+        Page<Member> page = memberRepository.findByAge(age , pageRequest);
+        Page<MemberDto> map = page.map(m -> new MemberDto(m.getId() , m.getUsername() , null));
+
+        //then
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+
+    }
 
 }
